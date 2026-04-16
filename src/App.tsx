b3e139dc7d3162aa6942e624957574d508b15edb
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-16 15:44:40 +1000

    Build a simple countdown timer with mm:ss format

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const formatter = (n: number) => String(n).padStart(2, "0");
+
 function App() {
+  const [second, setSecond] = useState(60);
+
+  useEffect(() => {
+    const secondTimer = setInterval(() => {
+      setSecond(second - 1);
+    }, 1000);
+
+    return () => clearInterval(secondTimer);
+  });
+
+  const transformedSecond = formatter(second % 60);
+  const transformedMinute = formatter(Math.floor(second / 60));
+
   return (
     <>
       <h1>Pomodoro App</h1>
+      <p>
+        {transformedMinute}:{transformedSecond}
+      </p>
+      <p></p>
     </>
   );
 }
