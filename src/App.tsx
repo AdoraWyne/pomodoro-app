44d5c0e99f463f61ee0c713a5426169157c8e464
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-18 17:30:03 +1000

    stop running useEffect when second is equal or less than 0.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import "./App.css";
 const formatter = (n: number) => String(n).padStart(2, "0");
 
 function App() {
-  const [second, setSecond] = useState(60);
+  const [second, setSecond] = useState(5);
 
   useEffect(() => {
+    if (second <= 0) return;
+
     const secondTimer = setInterval(() => {
       setSecond(second - 1);
     }, 1000);
 
     return () => clearInterval(secondTimer);
-  });
+  }, [second]);
 
   const transformedSecond = formatter(second % 60);
   const transformedMinute = formatter(Math.floor(second / 60));
@@ -23,7 +25,6 @@ function App() {
       <p>
         {transformedMinute}:{transformedSecond}
       </p>
-      <p></p>
     </>
   );
 }
