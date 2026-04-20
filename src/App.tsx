93e81c30dc733dbf67b891f3558b5b3ba4191ae3
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-20 18:16:46 +1000

    Add pause the timer functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,32 @@ const transformedTimer = (second: number) => {
 
 function App() {
   const [now, setNow] = useState(() => Date.now()); // constantly moving
-  const [end, setEnd] = useState(() => Date.now() + 5000); // fixed , use useEnd when reset
+  const [end, setEnd] = useState(() => Date.now() + 60_000); // fixed , use useEnd when reset
+  const [pause, setPause] = useState(false);
 
   const remainingSeconds = (end - now) / 1000;
   const isRunning = remainingSeconds > 0;
   const second = Math.ceil(Math.max(0, end - now) / 1000);
 
+  const handlePause = () => {
+    setPause(!pause);
+  };
+
   useEffect(() => {
-    if (!isRunning) return;
+    if (!isRunning || pause) return;
 
     const secondTimer = setInterval(() => {
       setNow(Date.now());
     }, 100);
 
     return () => clearInterval(secondTimer);
-  }, [isRunning]);
+  }, [isRunning, pause]);
 
   return (
     <>
       <h1>Pomodoro App</h1>
       <p>{transformedTimer(second)}</p>
+      <button onClick={handlePause}>Pause</button>
     </>
   );
 }
