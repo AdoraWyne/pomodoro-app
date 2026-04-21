cb49ec146c7af5c587acdffff3eb8a488bfb9598
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-21 23:20:20 +1000

    Rename constants to better name

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BREAK_SECONDS = 300_000; // 5mins
 export const LONG_BREAK_SECONDS = 1_200_000; // 20mins
-export const TESTING_SECONDS = 60_000; // 1 min
-export const WORKING_SECONDS = 1_500_000; // 25mins
+export const INITIAL_SECONDS = 60_000; // 1 min
+export const FOCUS_SECONDS = 1_500_000; // 25mins
